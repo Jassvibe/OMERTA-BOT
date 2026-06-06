97885ac874be4d49126d6e927f8a3e3a2811a2fa
Jassvibe <gamermob8585@gmail.com>
2026-06-06 08:32:50 +0530

    Add files via upload

--- db.py
+++ db.py
@@ -5,3 +5,21 @@ def get_connection():
     return psycopg2.connect(
         os.getenv("DATABASE_URL")
     )
+
+def test_connection():
+    try:
+        conn = get_connection()
+        cur = conn.cursor()
+
+        cur.execute("SELECT 1")
+
+        result = cur.fetchone()
+
+        cur.close()
+        conn.close()
+
+        return result[0] == 1
+
+    except Exception as e:
+        print(f"Database Error: {e}")
+        return False
