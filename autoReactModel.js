47535bf0471a460d9d9d216b03f45def8913dfe1
Jassvibe <gamermob8585@gmail.com>
2026-06-07 10:45:47 +0530

    Create autoReactModel.js

---
+++ autoReactModel.js
@@ -0,0 +1,24 @@
+hereconst mongoose =
+require("mongoose");
+
+const schema =
+new mongoose.Schema({
+
+ guildId:String,
+
+ trigger:String,
+
+ emojis:[String],
+
+ exact:{
+  type:Boolean,
+  default:false
+ }
+
+});
+
+module.exports =
+mongoose.model(
+"AutoReact",
+schema
+);
