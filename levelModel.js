c8668fc64b602b758a9ae3a38f17a204d2a6adbb
Jassvibe <gamermob8585@gmail.com>
2026-06-07 10:41:40 +0530

    Create levelModel.js

---
+++ levelModel.js
@@ -0,0 +1,27 @@
+const mongoose =
+require("mongoose");
+
+const levelSchema =
+new mongoose.Schema({
+
+ guildId:String,
+
+ userId:String,
+
+ xp:{
+  type:Number,
+  default:0
+ },
+
+ level:{
+  type:Number,
+  default:1
+ }
+
+});
+
+module.exports =
+mongoose.model(
+"Level",
+levelSchema
+);
