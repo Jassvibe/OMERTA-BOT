e1e4fe6f571828ef9c3d79672f8260dbf9057e09
Jassvibe <gamermob8585@gmail.com>
2026-06-08 09:39:41 +0530

    Create settingsCommands.js

---
+++ settingsCommands.js
@@ -0,0 +1,177 @@
+hereconst GuildSettings =
+require("./guildSettings");
+
+const AutoResponse =
+require("./autoResponseModel");
+
+const AutoReact =
+require("./autoReactModel");
+
+module.exports =
+async (interaction)=>{
+
+ if(
+ interaction.commandName
+ ===
+ "setlog"
+ ){
+
+  const channel =
+  interaction.options.getChannel(
+  "channel"
+  );
+
+  let settings =
+  await GuildSettings.findOne({
+
+   guildId:
+   interaction.guild.id
+
+  });
+
+  if(!settings){
+
+   settings =
+   await GuildSettings.create({
+
+    guildId:
+    interaction.guild.id
+
+   });
+
+  }
+
+  settings.logChannel =
+  channel.id;
+
+  await settings.save();
+
+  return interaction.reply({
+
+   content:
+   `Log Channel Set To ${channel}`
+
+  });
+
+ }
+
+ if(
+ interaction.commandName
+ ===
+ "setwelcome"
+ ){
+
+  const channel =
+  interaction.options.getChannel(
+  "channel"
+  );
+
+  let settings =
+  await GuildSettings.findOne({
+
+   guildId:
+   interaction.guild.id
+
+  });
+
+  if(!settings){
+
+   settings =
+   await GuildSettings.create({
+
+    guildId:
+    interaction.guild.id
+
+   });
+
+  }
+
+  settings.welcomeChannel =
+  channel.id;
+
+  await settings.save();
+
+  return interaction.reply({
+
+   content:
+   `Welcome Channel Set To ${channel}`
+
+  });
+
+ }
+
+ if(
+ interaction.commandName
+ ===
+ "addresponse"
+ ){
+
+  const trigger =
+  interaction.options.getString(
+  "trigger"
+  );
+
+  const response =
+  interaction.options.getString(
+  "response"
+  );
+
+  await AutoResponse.create({
+
+   guildId:
+   interaction.guild.id,
+
+   trigger,
+
+   response
+
+  });
+
+  return interaction.reply({
+
+   content:
+   "Response Added"
+
+  });
+
+ }
+
+ if(
+ interaction.commandName
+ ===
+ "addreaction"
+ ){
+
+  const trigger =
+  interaction.options.getString(
+  "trigger"
+  );
+
+  const emoji =
+  interaction.options.getString(
+  "emoji"
+  );
+
+  await AutoReact.create({
+
+   guildId:
+   interaction.guild.id,
+
+   trigger,
+
+   emojis:[
+   emoji
+   ]
+
+  });
+
+  return interaction.reply({
+
+   content:
+   "Reaction Added"
+
+  });
+
+ }
+
+};
