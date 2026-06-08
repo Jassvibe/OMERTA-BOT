79460d10332c190a1c6e8e6e514659432c09c5b9
Jassvibe <gamermob8585@gmail.com>
2026-06-08 09:23:48 +0530

    Update autoReactModel.js

--- autoReactModel.js
+++ autoReactModel.js
@@ -1,24 +1,22 @@
-hereconst mongoose =
-require("mongoose");
+const mongoose = require("mongoose");
 
-const schema =
-new mongoose.Schema({
+const schema = new mongoose.Schema({
 
- guildId:String,
+ guildId: String,
 
- trigger:String,
+ trigger: String,
 
- emojis:[String],
+ emojis: [String],
 
- exact:{
-  type:Boolean,
-  default:false
+ exact: {
+  type: Boolean,
+  default: false
  }
 
 });
 
 module.exports =
 mongoose.model(
-"AutoReact",
-schema
+ "AutoReact",
+ schema
 );
