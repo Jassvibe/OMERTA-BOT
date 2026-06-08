68571c8494a300d3e7a52645afe6d5fb832e2dd3
Jassvibe <gamermob8585@gmail.com>
2026-06-08 09:37:31 +0530

    Update slashCommands.js

--- slashCommands.js
+++ slashCommands.js
@@ -8,4 +8,60 @@ module.exports = [
   new SlashCommandBuilder()
     .setName("help")
     .setDescription("Show help menu")
-];
+new SlashCommandBuilder()
+.setName("setlog")
+.setDescription("Set log channel")
+
+.addChannelOption(option =>
+ option
+ .setName("channel")
+ .setDescription("Log Channel")
+ .setRequired(true)
+),
+
+new SlashCommandBuilder()
+.setName("setwelcome")
+.setDescription("Set welcome channel")
+
+.addChannelOption(option =>
+ option
+ .setName("channel")
+ .setDescription("Welcome Channel")
+ .setRequired(true)
+),
+
+new SlashCommandBuilder()
+.setName("addresponse")
+.setDescription("Add auto response")
+
+.addStringOption(option =>
+ option
+ .setName("trigger")
+ .setDescription("Trigger")
+ .setRequired(true)
+)
+
+.addStringOption(option =>
+ option
+ .setName("response")
+ .setDescription("Response")
+ .setRequired(true)
+),
+
+new SlashCommandBuilder()
+.setName("addreaction")
+ .setDescription("Add auto reaction")
+
+ .addStringOption(option =>
+ option
+ .setName("trigger")
+ .setDescription("Trigger")
+ .setRequired(true)
+ )
+
+ .addStringOption(option =>
+ option
+ .setName("emoji")
+ .setDescription("Emoji")
+ .setRequired(true)
+ )];
