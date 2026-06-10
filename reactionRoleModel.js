4ce7e7304e380f3d801a7eba54cfbea0240ebc9a
Jassvibe <gamermob8585@gmail.com>
2026-06-10 07:50:17 +0530

    Create reactionRoleModel.js

---
+++ reactionRoleModel.js
@@ -0,0 +1,21 @@
+const mongoose =
+require("mongoose");
+
+const schema =
+new mongoose.Schema({
+
+ guildId: String,
+
+ messageId: String,
+
+ emoji: String,
+
+ roleId: String
+
+});
+
+module.exports =
+mongoose.model(
+ "ReactionRole",
+ schema
+);￼Enter
